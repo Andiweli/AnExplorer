f880e551b389f58a1011272cb5be2bc83a40aca1
Andiweli <stuermer.andreas@gmail.com>
2026-09-13 21:10:08 +0200

    Start FTP service through foreground-service API

--- app/src/main/java/dev/dworks/apps/anexplorer/receiver/ConnectionsReceiver.java
+++ app/src/main/java/dev/dworks/apps/anexplorer/receiver/ConnectionsReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.core.content.ContextCompat;
+
 import dev.dworks.apps.anexplorer.misc.ConnectionUtils;
 import dev.dworks.apps.anexplorer.misc.NotificationUtils;
 import dev.dworks.apps.anexplorer.service.ConnectionsService;
@@ -23,22 +25,29 @@ public class ConnectionsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (intent == null) {
+            return;
+        }
+
         final String action = intent.getAction();
         if (ACTION_START_FTPSERVER.equals(action)) {
             Intent serverService = new Intent(context, ConnectionsService.class);
-            serverService.putExtras(intent.getExtras());
+            if (intent.getExtras() != null) {
+                serverService.putExtras(intent.getExtras());
+            }
             if (!ConnectionUtils.isServerRunning(context)) {
-                context.startService(serverService);
+                ContextCompat.startForegroundService(context, serverService);
             }
         } else if (ACTION_STOP_FTPSERVER.equals(action)) {
             Intent serverService = new Intent(context, ConnectionsService.class);
-            serverService.putExtras(intent.getExtras());
+            if (intent.getExtras() != null) {
+                serverService.putExtras(intent.getExtras());
+            }
             context.stopService(serverService);
         } else if (ACTION_FTPSERVER_STARTED.equals(action)) {
             NotificationUtils.createFtpNotification(context, intent, FTP_NOTIFICATION_ID);
         } else if (ACTION_FTPSERVER_STOPPED.equals(action)) {
             NotificationUtils.removeNotification(context, FTP_NOTIFICATION_ID);
         }
-
     }
 }
